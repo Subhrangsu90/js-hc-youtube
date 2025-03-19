a9bd678e8357ed03b8d933f32615d314b23ade8b
Subhrangsu90 <subhrangsubera@outlook.com>
2025-03-19 15:19:51 +0530

    Comment out console.log statements and add examples illustrating primitive vs reference data types in JavaScript

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -11,7 +11,7 @@ let userEmail;
 
 const id = Symbol("123");
 const anotherId = Symbol("123");
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 const bigNumber = 3456543576654356754n;
 
@@ -32,6 +32,30 @@ const myFunction = function () {
 	console.log("Hello World");
 };
 
-console.log(typeof heros);
+// console.log(typeof heros);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive) => Copy, Heap (Non-Primitive) => Reference
+
+let myYoutubeName = "Subhrangshuberacom";
+
+let anotherName = myYoutubeName;
+anotherName = "Subhrangshu";
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let userOne = {
+	email: "user@google.com",
+	upi: "user@ybl",
+};
+
+let userTwo = userOne;
+
+userTwo.email = "subhrangshu@google.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
